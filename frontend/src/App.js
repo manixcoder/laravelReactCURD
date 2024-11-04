d1ead2e6ef0ec893dfea65af63c4244311716137
manixcoder <pathakmanish86@gmail.com>
2024-11-04 21:48:40 +0530

    First commit for laravel and react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar/NavBar";
+import Users from "./components/Users/Users";
+
+
+function App() {
+  return (
+    <>
+    <NavBar/>
+    <Users/>
+    </>
+  );
+}
+
+export default App;
